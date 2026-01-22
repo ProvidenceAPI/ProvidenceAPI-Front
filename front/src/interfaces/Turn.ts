3ba47a02a1b27908f44c157a39620e8e83549507
SofiaBartoli <sofiadbartoli@gmail.com>
2026-01-22 11:13:06 -0300

    Agregue la relacion de turnos con actividades

--- front/src/interfaces/Turn.ts
+++ front/src/interfaces/Turn.ts
@@ -3,9 +3,16 @@ export interface Turn {
   activityId: string;
   date: string;
   startTime: string;
+  endTime: string;
   capacity: number;
   availableSpots: number;
   status?: string;
+  isFreeTrial?: boolean;
+  activity?: {
+    id: string;
+    name: string;
+    duration: number;
+  };
   createdAt?: string;
   updatedAt?: string;
 }
